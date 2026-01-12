0a8f621ca2d60d97d94427cdae2f38338f565477
Hanumanthareddy <reddyreddy884@gmail.com>
2026-01-12 21:29:25 +0530

    add html

--- cart/urls.py
+++ cart/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('',views.cart_home, name='cart-home'),
     path('cartlist/',views.cart_details, name='list-name'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+def cart_home(request):
+    template = loader.get_template('first.html')
+    return HttpResponse(template.render())
 
 def cart_details(request):
     return HttpResponse("Hello Word")
